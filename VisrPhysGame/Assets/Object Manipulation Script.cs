b08e5a09cc8a854ec7d06561555acfbfef31f333
shed93 <40423465+shed93@users.noreply.github.com>
2018-06-25 17:07:49 +0100

    Added Object Manipulation

---
+++ VisrPhysGame/Assets/Object Manipulation Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulationScript : MonoBehaviour {
+
+	void rotateX(Transform gameObject,int angle)
+    {
+        gameObject.Rotate(Vector3.right*Time.deltaTime*angle);
+    }
+    void rotateY(Transform gameObject, int angle)
+    {
+        gameObject.Rotate(Vector3.up * Time.deltaTime * angle);
+    }
+    void rotateZ(Transform gameObject, int angle)
+    {
+        gameObject.Rotate(0, 0, Time.deltaTime*angle);
+    }
+    void translateX(Transform gameobject,int step)
+    {
+        gameObject.transform.Translate(Vector3.forward*Time.deltaTime*step);
+    }
+}
