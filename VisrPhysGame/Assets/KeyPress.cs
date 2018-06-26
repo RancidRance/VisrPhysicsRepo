15ba787d9ad86cae9fdf2fcd108c9d74667645bc
Ahmed El Ghazawy <ghezo96@gmail.com>
2018-06-26 10:31:21 +0100

    Main menu
    Added basic main menu scene with only 2 text fields and a key press detect funtion, which will be edited once keys are added to the system and another scene is added.

---
+++ VisrPhysGame/Assets/KeyPress.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPress : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey)
+        {
+            Debug.Log("something has been pressed");
+        }
+	}
+}
