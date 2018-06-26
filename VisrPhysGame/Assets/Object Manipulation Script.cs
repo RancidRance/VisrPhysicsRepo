b3d05bf5322fdd576aa32f59d824ded4e279b8ff
Ahmed El Ghazawy <ghezo96@gmail.com>
2018-06-26 11:56:50 +0100

    Text fields modifying script

--- VisrPhysGame/Assets/Object Manipulation Script.cs
+++ VisrPhysGame/Assets/Object Manipulation Script.cs
@@ -1,59 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ObjectManipulationScript : MonoBehaviour {
-    public Text manipulationType;
-    public Text manipulationAxis;
-
-    private void Start()
-    {
-        manipulationType.text = "Manipulation Type: ";
-        manipulationAxis.text = "Manipulation Axis: ";
-    }
 
     void rotateX(Transform gameObject,float angle)
     {
         gameObject.Rotate(Vector3.right*Time.deltaTime*angle);
-        manipulationType.text = "Manipulation Type: Rotation";
-        manipulationAxis.text = "Manipulation Axis: X";
 
     }
     void rotateY(Transform gameObject, float angle)
     {
         gameObject.Rotate(Vector3.up * Time.deltaTime * angle);
-        manipulationType.text = "Manipulation Type: Rotation";
-        manipulationAxis.text = "Manipulation Axis: Y";
     }
     void rotateZ(Transform gameObject, float angle)
     {
         gameObject.Rotate(Vector3.forward * Time.deltaTime*angle);
-        manipulationType.text = "Manipulation Type: Rotation";
-        manipulationAxis.text = "Manipulation Axis: Z";
     }
     void translateX(Transform gameobject,float step)
     {
         gameObject.transform.Translate(Vector3.forward*Time.deltaTime*step);
-        manipulationType.text = "Manipulation Type: Translation";
-        manipulationAxis.text = "Manipulation Axis: X";
     }
     void translateY(Transform gameobject, float step)
     {
         gameObject.transform.Translate(Vector3.right * Time.deltaTime * step);
-        manipulationType.text = "Manipulation Type: Translation";
-        manipulationAxis.text = "Manipulation Axis: Y";
     }
     void translateZ(Transform gameobject, float step)
     {
         gameObject.transform.Translate(Vector3.down * Time.deltaTime * step);
-        manipulationType.text = "Manipulation Type: Translation";
-        manipulationAxis.text = "Manipulation Axis: Z";
     }
     void scale(Transform gameobject,float step)
     {
         gameObject.transform.localScale.Scale(new Vector3(1,1,1)*Time.deltaTime * step);
-        manipulationType.text = "Manipulation Type: Scaling";
-        manipulationAxis.text = "";
     }
 }
---
+++ VisrPhysGame/Assets/TextEditorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextEditorScript : MonoBehaviour {
+
+    public Text manipulationType;
+    public Text manipulationAxis;
+
+    // Use this for initialization
+    private void Start()
+    {
+        manipulationType.text = "";
+        manipulationAxis.text = "";
+    }
+
+    public void setManipulationToRotation()
+    {
+        manipulationType.text = "Manipulation Type: Rotation";
+    }
+
+    public void setManipulationToTranslation()
+    {
+        manipulationType.text = "Manipulation Type: Translation";
+    }
+
+    public void setManipulationToScaling()
+    {
+        manipulationType.text = "Manipulation Type: Scaling";
+    }
+
+    public void setAxisToX()
+    {
+        manipulationAxis.text = "Manipulation Axis: X";
+    }
+
+    public void setAxisToY()
+    {
+        manipulationAxis.text = "Manipulation Axis: Y";
+    }
+
+    public void setAxisToZ()
+    {
+        manipulationAxis.text = "Manipulation Axis: Z";
+    }
+}
