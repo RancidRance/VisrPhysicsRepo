102b5eb0da076153f47ba3ca8f07d3495d4ded6e
shed93 <40423465+shed93@users.noreply.github.com>
2018-06-26 10:54:19 +0100

    Added manipulation functions and raycast grab

--- VisrPhysGame/Assets/Object Manipulation Script.cs
+++ VisrPhysGame/Assets/Object Manipulation Script.cs
@@ -4,20 +4,32 @@ using UnityEngine;
 
 public class ObjectManipulationScript : MonoBehaviour {
 
-	void rotateX(Transform gameObject,int angle)
+	void rotateX(Transform gameObject,float angle)
     {
         gameObject.Rotate(Vector3.right*Time.deltaTime*angle);
     }
-    void rotateY(Transform gameObject, int angle)
+    void rotateY(Transform gameObject, float angle)
     {
         gameObject.Rotate(Vector3.up * Time.deltaTime * angle);
     }
-    void rotateZ(Transform gameObject, int angle)
+    void rotateZ(Transform gameObject, float angle)
     {
-        gameObject.Rotate(0, 0, Time.deltaTime*angle);
+        gameObject.Rotate(Vector3.forward * Time.deltaTime*angle);
     }
-    void translateX(Transform gameobject,int step)
+    void translateX(Transform gameobject,float step)
     {
         gameObject.transform.Translate(Vector3.forward*Time.deltaTime*step);
+    }
+    void translateY(Transform gameobject, float step)
+    {
+        gameObject.transform.Translate(Vector3.right * Time.deltaTime * step);
+    }
+    void translateZ(Transform gameobject, float step)
+    {
+        gameObject.transform.Translate(Vector3.down * Time.deltaTime * step);
+    }
+    void scale(Transform gameobject,float step)
+    {
+        gameObject.transform.localScale.Scale(new Vector3(1,1,1)*Time.deltaTime * step);
     }
 }
---
+++ VisrPhysGame/Assets/RayCastGrab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastGrab : MonoBehaviour {
+
+    public Camera playerHead;
+    RaycastHit hit;
+    public int layerMask = 2;
+    
+    Transform rayCastGrab()
+    {
+        if(Physics.Raycast(playerHead.transform.position,playerHead.transform.TransformDirection(Vector3.forward),out hit, Mathf.Infinity, layerMask))
+        {
+            return hit.transform;
+        }
+        return null;
+    }
+}
