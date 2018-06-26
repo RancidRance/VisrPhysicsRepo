868a6ebcde28bab86fc3c10822b0d7e8d727a688
RancidRance <36491626+RancidRance@users.noreply.github.com>
2018-06-26 11:52:02 +0100

    Added Player movement

---
+++ VisrPhysGame/Assets/ControllerInput.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetAxisRaw("PlayerForwardBack") < 0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().MoveForward();
+            print("Forward");
+        }
+
+        if (Input.GetAxisRaw("PlayerForwardBack") > -0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().MoveBack();
+        }
+
+        if (Input.GetAxisRaw("PlayerTurn") > 0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().TurnRight();
+        }
+
+        if (Input.GetAxisRaw("PlayerTurn") < -0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().TurnLeft();
+        }
+
+      //  if (Input.GetAxisRaw("CameraX") > 0.3)
+      //  {
+    //        gameObject.GetComponent<PlayerMovement>().MoveCameraXRight();
+    //    }
+
+    //    if (Input.GetAxisRaw("CameraX") < -0.3)
+   //     {
+   //         gameObject.GetComponent<PlayerMovement>().MoveCameraXLeft();
+  //      }
+
+        if (Input.GetAxisRaw("CameraY") > 0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().MoveCameraYUp();
+        }
+
+        if (Input.GetAxisRaw("CameraY") < -0.3)
+        {
+            gameObject.GetComponent<PlayerMovement>().MoveCameraYDown();
+        }
+
+        if (Input.GetButtonDown("Select") == true)
+        {
+
+        }
+
+        if (Input.GetButtonDown("Deselect") == true)
+        {
+
+        }
+
+        if (Input.GetButtonDown("ChangeModeForward") == true)
+        {
+
+        }
+
+        if (Input.GetButtonDown("ChangeModeBack") == true)
+        {
+
+        }
+
+        if (Input.GetAxisRaw("Increase") > 0.3)
+        {
+
+        }
+
+        if (Input.GetAxisRaw("Decrease") > 0.3)
+        {
+
+        }
+
+
+    }
+}
---
+++ VisrPhysGame/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public GameObject player;
+    public int movementSpeed;
+    public int rotateSpeed;
+    public Camera playerCamera;
+
+    // Use this for initialization
+    void Start () {
+       playerCamera = gameObject.GetComponentInChildren(typeof(Camera)) as Camera;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+
+	}
+
+
+   public void MoveForward ()
+    {
+        player.transform.position += transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+   public void MoveBack()
+    {
+        player.transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+   public void TurnLeft()
+    {
+        player.transform.Rotate(-Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+
+   public void TurnRight()
+    {
+        player.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+
+   public void MoveCameraXLeft()
+    {
+        playerCamera.transform.Rotate(-Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+
+   public void MoveCameraXRight()
+    {
+        playerCamera.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
+    }
+
+   public void MoveCameraYUp()
+    {
+        playerCamera.transform.Rotate(-Vector3.left * Time.deltaTime * rotateSpeed);
+    }
+
+   public void MoveCameraYDown()
+    {
+        playerCamera.transform.Rotate(Vector3.left * Time.deltaTime * rotateSpeed);
+    }
+
+
+
+
+}
