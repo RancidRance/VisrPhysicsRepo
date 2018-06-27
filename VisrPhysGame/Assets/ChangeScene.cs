1802dcea23594d61e64845000ecd7f9870579e51
Ahmed El Ghazawy <ghezo96@gmail.com>
2018-06-27 15:36:09 +0100

    Controls scene added alongside fixing the pointer problem.

---
+++ VisrPhysGame/Assets/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(10f);
+    }
+
+	// Use this for initialization
+	void Start () {
+        SwitchScene();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+	}
+}
