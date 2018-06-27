a9c5d843548be00bc249d9a30def6cfeec88524d
shed93 <40423465+shed93@users.noreply.github.com>
2018-06-27 15:42:54 +0100

    added exclusion list script

---
+++ VisrPhysGame/Assets/exclusionList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exclusionList : MonoBehaviour {
+    List<string> pExclusionList = new List<string>() {
+        "8e6bcda9-856e-441b-9c24-4169a9843b57",
+        "cb00f27f-d492-406b-9a1d-0bbe1377f41a" };
+	public bool exclusionListCheck(string ID)
+    {
+        if (pExclusionList.Contains(ID))
+        {
+            return true;
+        }
+        return false;
+    }
+}
