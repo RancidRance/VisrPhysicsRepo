bda9fcdc34bf9cb4714b6eceff2bea13e93db6b7
RancidRance <36491626+RancidRance@users.noreply.github.com>
2018-06-29 15:37:27 +0100

    Dodgeball

---
+++ VisrPhysGame/Assets/DodgeballDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeballDetection : MonoBehaviour {
+
+    // Use this for initialization
+    public AudioSource source;
+    public AudioClip hitSound;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "SceneLinkMoveableObject")
+        {
+            source.PlayOneShot(hitSound, 1);
+        }
+    }
+
+}
