c7b1039d2734aced8d867b1eaea3c085137820ab
RancidRance <36491626+RancidRance@users.noreply.github.com>
2018-06-29 12:20:03 +0100

    PlayerAvatar

--- VisrPhysGame/Assets/AssignPlayerAvatars.cs
+++ VisrPhysGame/Assets/AssignPlayerAvatars.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class AssignPlayerAvatars : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+
+     float nextActionTime = 0.0f;
+     float period = 0.5f;
+    public GameObject PlayerAvatar;
+    
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+
+
+            GameObject viewPoints = GameObject.Find("Viewpoints");
+
+            Transform[] children = viewPoints.GetComponentsInChildren<Transform>();
+            foreach (Transform child in children)
+            {
+                if (child.transform.childCount < 1 && child.name != "TempAvatar(Clone)")
+                {
+                    Instantiate(PlayerAvatar, child.transform);
+                }
+          
+            }
+
+        }
+   }
 }
